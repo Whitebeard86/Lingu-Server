af23da510c01f707686695c1c63482f0e3e544df
Luoen <mluoen@gmail.com>
2015-11-20 22:08:21 +0000

    Socket login system; Room parameters;
    Sistema de login atraves de socket.io,
    Estrutura de room de jogos.

--- core.js
+++ core.js
@@ -1,22 +1,55 @@
 var ACTIONS = {
     REGISTER: 1,
-    LOGIN: 2
+    LOGIN: 2,
+
 };
 
-var DATABASE = {
-    MYSQL: {
+var DATABASE = mysql.createConnection({
         HOST_NAME: "anlagehub.com",
         DATABASE: "anlageme_lingu",
         USER: "anlageme_lingu",
         PASSWORD: "agrav_lingu22!"
-    }
-};
+    });
 
-var io = require('socket.io')(8080);
-io.on('connection', function (socket) {
-    socket.on('message', function(message) { onMessageReceived(message, socket); });
-    socket.on('disconnect', function () {});
+DATABASE.connect(function(err) {
+    if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+}});
+
+// Let’s make node/socketio listen on port 8080
+var io = require('socket.io').listen(8080);
+ 
+io.sockets.on('connection', function(socket){
+    socket.auth = false;
+    //temp delete socket from namespace connected map
+    //delete socketio.sockets.connected[socket.id];
+    socket.on('disconnect', function() {
+        io.sockets.emit('users disconnected', socketCount);
+        onMessageReceived(data, socket); 
+    })
+ 
+    socket.on('sign_up', function(data){
+        io.sockets.emit('sign_up', data);
+        onMessageReceived(data, socket); 
+        
+    })
+
+    socket.on('authenticate', function(data){ 
+        socket.auth = false;
+        onMessageReceived(data, socket) 
+    })
+ 
+    //If the socket didn't authenticate, disconnect it
+    setTimeout(function(){
+        //If the socket didn't authenticate, disconnect it
+        if (!socket.auth) {
+          console.log("Disconnecting socket ", socket.id);
+          socket.disconnect('unauthorized');
+        }
+    }, 2000);
 });
+
 
 function onMessageReceived (msg, socket) {
     var request = JSON.parse(msg);
@@ -27,7 +60,10 @@ function onMessageReceived (msg, socket) {
 
 function processRequest (request, socket) {
     switch(request.action) {
-        case ACTIONS.LOGIN:
+        case 1:
+            handleSignup(request, socket);
+            break;
+        case 2:
             handleLogin(request, socket);
             break;
     }
@@ -35,4 +71,43 @@ function processRequest (request, socket) {
 
 function handleLogin(request, socket) {
     // handle login here..
+    // tratar db injection
+    db.query('SELECT * FROM players where username = ? and password = ?', request.username, request.password)
+        .on('result', function(data){
+            socket.auth = true;
+        })
+        .on('error', function(){
+            // Only emit notes after query has been completed
+            socket.emit('erros', data)
+        })
+    
 }
+
+function handleSignup(request, socket) {
+    // handle login here..
+    db.query('INSERT INTO player (username, password) VALUES (?,?)', request.username, request.password)
+        .on('result', function(data){
+
+            socket.emit('sucesso', data)
+        })
+        .on('error', function(){
+            socket.emit('erros', data)
+        })
+    
+}
+
+function handleLogout(request, socket) {
+    // handle login here..
+}
+
+
+function handlegamerequest(request, socket) {
+
+}
+
+
+
+
+
+
+
