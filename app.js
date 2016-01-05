8823aceff59e9f8bb860cc6d51c1b68fd0c33f8f
João Alves <joao.cpp.sca@gmail.com>
2016-01-05 12:26:19 +0000

    sync

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 var http = require('http');
-var io = require('socket.io')(SOCKET_PORT);
+var io = require('socket.io').listen(http);
 
 app.use(function (req, res, next) {
 
